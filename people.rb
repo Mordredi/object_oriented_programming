db35ab64c74a1900fb948e0686aaa3a2fd02f8c6
Arthur <arthur.p.wright@gmail.com>
2015-04-02 13:53:25 -0400

    Defined to_s with Person class

--- people.rb
+++ people.rb
@@ -2,15 +2,19 @@ class Person(name)
   def initialize
     @name = name
   end
+
+  def to_s
+    "Hi, my name is #{@name}"
+  end
 end
 
-class Student
+class Student < Person
   def learn
     puts "I get it!"
   end
 end
 
-class Instructor
+class Instructor < Person
   def teach
     puts "Everything in Ruby is an Object."
   end
