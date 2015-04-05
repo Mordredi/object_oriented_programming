2d48bb7d756d44e2ae46615ba7b6dded76c93923
Arthur <arthur.p.wright@gmail.com>
2015-04-05 17:58:47 -0400

    Figured out float and adding of products. Slight rounding issue on total tax.

--- tax.rb
+++ tax.rb
@@ -9,13 +9,7 @@ class ShoppingList
   end
 
   def shopping
-    answer = add_item
-    if answer == "yes"
-      answer =  add_item
-    elsif answer == "no"
-      calculate_tax
-      calculate_total
-    end
+    add_item
     puts to_s
   end
 
@@ -26,7 +20,7 @@ class ShoppingList
     puts "How many?"
     item << gets.chomp.to_i
     puts "What is the price?"
-    item << gets.to_i
+    item << gets.to_f
     puts "Please select type of item by number"
     puts "[1] food"
     puts "[2] book"
@@ -35,7 +29,13 @@ class ShoppingList
     item << gets.chomp.to_i
     @items << item
     puts "Would you like to add another item? Yes, or no"
-    gets.chomp.downcase
+    answer = gets.chomp.downcase
+    if answer == "yes"
+      add_item
+    elsif answer == "no"
+      calculate_tax
+      calculate_total
+    end
   end
 
   def to_s
@@ -48,16 +48,16 @@ class ShoppingList
   def calculate_tax
     @items.each do |item|
       if item[3] == 4
-        @tax += item[2]*item[1]*0.15
+        @tax += ((item[2]*100)*item[1]*0.15/100).round(2)
       elsif item[3] == 1 || item[3] == 2 || item[3] == 3
-        @tax += item[2]*item[1]*0.05
+        @tax += ((item[2]*100)*item[1]*0.05/100).round(2)
       end
     end
   end
 
   def calculate_total
     @items.each do |item|
-      @total += item[2]*item[1]
+      @total += ((item[2]*100)*item[1]/100).round(2)
     end
     @total += @tax
   end
